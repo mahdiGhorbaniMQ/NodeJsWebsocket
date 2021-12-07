1fa4156e204a9ecfed6212ff806639307f81ed1e
git/ <you@example.com>
2021-12-07 13:01:03 +0330

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8081 });
+  
+wss.on('connection', ws => {
+  onConnection(ws);
+  ws.on('message', message => {
+    onMessage(message, ws);
+  });
+  ws.on('error', error => {
+    OnError(error);
+  });
+   ws.on('close', ws=> {
+    onClose();
+})
+});
+
